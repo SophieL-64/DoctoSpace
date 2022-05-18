99ac4c87466481caa516192d37384d2ee71c6972
SophieL-64 <sophie.lamant31@gmail.com>
2022-05-18 17:35:17 +0200

    toggle

--- index.js
+++ index.js
@@ -1,52 +1,42 @@
-console.log("DOCTOSPACE")
+console.log("DOCTOSPACE");
 
+let i = 1;
+setInterval(() => {
+  console.log("test interval toutes les 3 secondes");
+  permute(i);
+  if (i !== 3) {
+    i++;
+  } else {
+    i = 1;
+  }
+}, 3000);
 
-let i=1;
-setInterval(()=>{
-    console.log("test interval toutes les 3 secondes");
-    permute(i)
-    if(i!==3){
-       i++; 
-    }
-    else{
-        i=1;
-    }
-}, 3000)
+function permute(i) {
+  let article1 = document.getElementById("testimonial-1");
+  let article2 = document.getElementById("testimonial-2");
+  let article3 = document.getElementById("testimonial-3");
 
+  if (i == 1) {
+    article3.classList.toggle("actif");
+    article1.classList.toggle("actif");
+    console.log("test----", article1);
+    console.log("cas: ", 1);
+  }
+  if (i == 2) {
+    article1.classList.toggle("actif");
+    article2.classList.toggle("actif");
+    console.log("cas: ", 2);
+  }
+  if (i == 3) {
+    article2.classList.toggle("actif");
+    article3.classList.toggle("actif");
+    console.log("cas: ", 3);
+  }
 
-
-function permute(i){
-    let article1=document.getElementById("testimonial-1");
- let article2=document.getElementById("testimonial-2");
- let article3=document.getElementById("testimonial-3");
-
- 
-    if (i==1){
-    article1.className="actif"
-    article2.className="inactif"
-    article3.className="inactif"
-        console.log("cas: ",1)
-    }
-      if (i==2){
-    article1.className="inactif"
-    article2.className="actif"
-    article3.className="inactif"
-            console.log("cas: ",2)
-
-    }
-      if (i==3){
-    article1.className="inactif"
-    article2.className="inactif"
-    article3.className="actif"
-            console.log("cas: ",3)
-    }
-
-    console.log(i,article1,article2,article3)
+  console.log(i, article1, article2, article3);
 }
 
-
 // console.log(article1,article2,article3)
-
 
 // peu pas sur interfacfe HTMLelement deplacer l'odre./.
 
@@ -54,25 +44,16 @@ function permute(i){
 
 // shiftRight(articles);
 
-
 //modifier l'odre du dom
 // articles.unshift();
 
-
-
-
-
-function shiftRight(arr){
-    arr.unshift(arr[arr.length]);
-    arr.pop();
-}
-
+// function shiftRight(arr) {
+//   arr.unshift(arr[arr.length]);
+//   arr.pop();
+// }
 
 //     articles.shift(articles[articles.length])
 //  console.log(articles);
-
-
-
 
 //  function perm(xs) {
 //   let ret = [];
@@ -90,4 +71,3 @@ function shiftRight(arr){
 //   }
 //   return ret;
 // }
-
