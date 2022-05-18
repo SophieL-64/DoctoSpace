b787b20773820f53d869d948866fbde8ac1e2234
kinotonik <zedmed@kinotonik.fr>
2022-05-18 17:16:01 +0200

    add parallax effect + sticky navbar + title<h1> & slogan <h2>

--- nav-js.js
+++ nav-js.js
@@ -1,21 +1,3 @@
-// function toggle() {
-//     document.getElementsById("sous__menu").classList.toggle("show");
-//   }
-  
-//   // Close the dropdown if the user clicks outside of it
-//   window.onclick = function(event) {
-//     if (!event.target.matches('.nav__sous__itemdrop')) {
-//       var dropdowns = document.getElementsByClassName("nav__sous__itemdrop");
-//       var i;
-//       for (i = 0; i < dropdowns.length; i++) {
-//         var openDropdown = dropdowns[i];
-//         if (openDropdown.classList.contains('show')) {
-//           openDropdown.classList.remove('show');
-//         }
-//       }
-//     }
-//   }
-
 
 function toggle() {
     let elm = document.getElementById('sousMenu');
