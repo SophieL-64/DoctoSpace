46c4cf8f9d76d00b996ef74afa1b7de1438bf7eb
kinotonik <zedmed@kinotonik.fr>
2022-05-18 19:31:16 +0200

    review index.html + add folder scr + little comment + not change nameclass

---
+++ scr/js/main.js
@@ -0,0 +1,23 @@
+//////////Navbar///////////////////////////
+
+// fonction toogle() pour faire apparaitre / disparaitre le sous-menu
+function toggle() {
+    let elm = document.getElementById('sousMenu');
+
+    if(elm.className === 'navSousItemdrop_active'){
+        elm.className = 'navSousItemdrop';
+    } else {
+        elm.className = 'navSousItemdrop_active';
+    }
+}
+
+//////////Caroussel/////////////////////////
+
+
+
+//////////Onglet Pathologie/////////////////////////
+
+
+
+//////////tesmoniale/////////////////////////
+
