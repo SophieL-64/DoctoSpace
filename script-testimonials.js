0b04bf854cd8aad3307fc93fbd6933fec4758f62
SophieL-64 <sophie.lamant31@gmail.com>
2022-05-18 14:46:26 +0200

    deleted script-testimonials.js

--- script-testimonials.js
+++
@@ -1,9 +0,0 @@
-
-const articleActive = Element.getElementsByClassName(.article), i;
-
-for (let i = 0; i < articleActive.length, i++) {
-  articleActive[i+1].style.display = 'none';
-  articleActive[i+2].style.display = 'none';
-}
-
-// setInterval(changeImage, 3000);
