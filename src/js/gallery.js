b8cb956c53d09e7a7924145f0720dc34d846275f
Dangocode <rilosjnthn@gmail.com>
2022-05-19 16:06:43 +0200

    folders, organization, added gallery-only css

---
+++ src/js/gallery.js
@@ -0,0 +1,14 @@
+gridGallery({
+    // gallery selector
+    selector: "#mSelector",
+    // enable dark mode
+    darkMode: true,
+    // or "horizontal"
+    layout: "square",
+    // space between images
+    gapLength: 4,
+    // row height
+    rowHeight: 180,
+    // column width
+    columnWidth: 200
+});
